21d1756a718026e31231053af495794ce4c34da1
Jeffrey Choate <jeff.lee.choate@gmail.com>
2021-01-24 21:29:08 -0800

    updated object to be uploaded to dynamoDb

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -13,9 +13,8 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
     console.log('createTodo.ts Processing event: ', event)//indicate I got into the method
 
     const itemId = uuid.v4()
-  
+    const timestamp = new Date().toISOString()
     const newTodo: CreateTodoRequest = JSON.parse(event.body)
-
 //TBD for checking if valid user was provided
     //const authorization = event.headers.Authorization
     //const split = authorization.split(' ')
@@ -30,10 +29,12 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
 
     //create JSON of data to be uploaded to dynamo DB
     const newItem = { 
-        id: itemId,
+        todoId: itemId,
+        createdAt: timestamp,
         userId: "Jeff",
-        ...newTodo
-    }
+        done: false,
+        ...newTodo//includes name and duedate
+    }//assume that attachmentURL will come via update only
 
     //actually write it to the dynamoDB
     await docClient.put({
