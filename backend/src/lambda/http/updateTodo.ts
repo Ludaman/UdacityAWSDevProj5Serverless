8a32635d3ea6a1afb4b915342762e94681c3e8be
Jeffrey Choate <jeff.lee.choate@gmail.com>
2021-01-25 14:55:43 -0800

    fixed serverless.yml file to have delete and update permissions for dynamoDB and added correct fields to updatetodos.ts

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -7,43 +7,34 @@ import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 
 const docClient = new AWS.DynamoDB.DocumentClient()
 const todosTable = process.env.TODOS_TABLE
-const todoIdIndex = process.env.INDEX_NAME
+//const todoIdIndex = process.env.INDEX_NAME
 
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const todoId = event.pathParameters.todoId
+    console.log('updateTodos.ts Processing event: ', event)
+
+    const todoId = event.pathParameters.todoId
+
+    console.log('Attempting to update todoID: ', todoId)
+
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
 
+  console.log('Parsed into: ', updatedTodo)
 
-  console.log('updateTodos.ts Processing event: ', event)
-//must update to use query instead when I start querying by userID
-  const result = await docClient.query({
-    TableName : todosTable,
-    IndexName : todoIdIndex,
-    KeyConditionExpression: 'todoId = :todoId',
-    ExpressionAttributeValues: {
-        ':todoId': todoId
-    }
-}).promise()
-
-console.log('updateTodos.ts found thid many matching items', result.Count)
-
-if(result.Count == 1){
-    // const replacementItem = { 
-    //     todoId: ,
-    //     createdAt: ,
-    //     userId: ,
-    //     attachmentURL: result.,
-    //     ...updatedTodo//includes name, duedate and done
-    // }//assume that attachmentURL will come via update only
     const newitem = await docClient.update({
         TableName: todosTable,
         Key: {
             userId: "Jeff",
-            todoId: todoId
+            todoId: "33"
         },
-        UpdateExpression: "set #name = :name, dueDate=:dueDate, done=:done",
-        ExpressionAttributeValues: updatedTodo
+        ExpressionAttributeNames: {"#name": "name"},
+        UpdateExpression: "set #name = :name, dueDate = :dueDate, done = :done",
+        ExpressionAttributeValues: {
+            ":name":"BOB",
+            ":dueDate":"anotehrnewDate",
+            ":done":"stillnotdone"
+        },
+        ReturnValues: "UPDATED_NEW"
       }).promise()
   
       //return to client application that write succeeded
@@ -57,17 +48,4 @@ if(result.Count == 1){
             newitem
         })
     }
-    
-}
-return {
-    statusCode: 404,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: 'Failed to update or find an item'
-  }
-
-
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-
 }
