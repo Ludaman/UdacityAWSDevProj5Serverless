031dedeca8d123b05b5fb995294889ca4a14c32d
Jeffrey Choate <jeff.lee.choate@gmail.com>
2021-01-25 12:03:52 -0800

    added basic get url.ts file

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,34 @@
-// import 'source-map-support/register'
+import 'source-map-support/register'
 
-// import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+const bucketName = process.env.IMAGES_S3_BUCKET
+const urlExpiration = process.env.SIGNED_URL_EXPIRATION
+import * as uuid from 'uuid'
+import * as AWS  from 'aws-sdk'
 
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   const todoId = event.pathParameters.todoId
 
-//   // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-//   return undefined
-// }
+const s3 = new AWS.S3({
+    signatureVersion: 'v4'
+  })
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  //const todoId = event.pathParameters.todoId
+  console.log('getULR.ts Processing event: ', event)
+  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
+
+  const imageId = uuid.v4()
+  const url = getUploadUrl(imageId)
+
+  return {
+    statusCode: 201,
+    body: url
+  }
+}
+
+function getUploadUrl(imageId: string) {
+    return s3.getSignedUrl('putObject', {
+      Bucket: bucketName,
+      Key: imageId,
+      Expires: urlExpiration
+    })
+}
