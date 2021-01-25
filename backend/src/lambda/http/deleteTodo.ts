b15d7ee28143b255e8f5f71be1cb8aa2f65059a3
Jeffrey Choate <jeff.lee.choate@gmail.com>
2021-01-25 11:06:42 -0800

    created initial delteTodo.ts

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,10 +1,36 @@
-// import 'source-map-support/register'
+import 'source-map-support/register'
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import * as AWS  from 'aws-sdk'
 
-// import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
 
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   const todoId = event.pathParameters.todoId
 
-//   // TODO: Remove a TODO item by id
-//   return undefined
-// }
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  console.log('deleteTodo.ts Processing event: ', event)
+
+  const todoId = event.pathParameters.todoId
+
+  console.log('attempting to delete ID: ', todoId)
+
+  await docClient.delete({
+      TableName: todosTable,
+      Key: {
+        userId: "Jeff",
+        todoId: todoId
+    }      
+  })
+
+  console.log('Deleted ID and returning status: ', todoId)
+
+
+  return {
+    statusCode: 201,
+    headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': true
+    },
+    body: "Deleted item"
+  }
+
+}
